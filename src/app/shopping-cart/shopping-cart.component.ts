0bd766922eebbfe23c24cc8941b12b90cda82996
Mohamed Gamal <Eng.mohamed.g.m@gmail.com>
2021-10-24 16:35:22 +0200

    Init shopping cart view

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,18 @@
+import { ShoppingCartService } from './../services/shopping-cart/shopping-cart.service';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCart } from '../models/shopping-cart';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.css']
+  styleUrls: ['./shopping-cart.component.css'],
 })
 export class ShoppingCartComponent implements OnInit {
+  cart$: Observable<ShoppingCart>;
+  constructor(private shoppingCart: ShoppingCartService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.cart$ = await this.shoppingCart.getCart();
   }
-
 }
